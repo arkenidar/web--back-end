6d2e469ba05d02050d02987ef230352e0e121c36
Dario Cangialosi Arkenidar <dario.cangialosi@gmail.com>
2024-02-06 13:33:44 +0100

    [EJS] <li> with columns from database (current data is a so called mock-up).

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   // // http://localhost:3000/
-  res.render('index', { title: 'Express nel corso NodeJS !!! restart 2' });
+
+  let items = [
+    { column1: "column1", column2: "column2" },
+    {
+      column1: "ExpressJS (from OpenJS foundation)",
+      column2: "HTTP server that is programmable by JavaScript"
+    },
+    {
+      column1: "PostgreSQL (BSD licensed)",
+      column2: "database server system"
+    },
+  ]
+  res.render('index', { items, title: 'Express nel corso NodeJS !!! restart 2' });
 });
 
 module.exports = router;
