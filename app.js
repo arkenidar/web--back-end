5aaf422ffacbc48633cb272787deb9d78089f831
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-07-21 18:50:48 +0200

    show errors always, not only in dev-env

--- app.js
+++ app.js
@@ -67,7 +67,8 @@ app.use(function (request, response, next) {
 app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = err
 
   // render the error page
   res.status(err.status || 500);
